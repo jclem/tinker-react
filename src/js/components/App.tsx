970d1cfe5c12b98f2d859e51df3d2fc602152e7f
Jonathan Clem <jclem@github.com>
2020-01-22 18:23:47 -0500

    Add React, jsx

---
+++ src/js/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world.</h1>
+      <style jsx>{`
+        h1 {
+          color: blue;
+        }
+      `}</style>
+    </div>
+  )
+}
